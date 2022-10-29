cb8f3e4ab446dad04333286d94537b65b4b76669
zalmek <zalmek23@gmail.com>
2022-10-29 15:10:18 +0300

    Labs

---
+++ lab2/main.py
@@ -0,0 +1,18 @@
+import numpy
+
+from lab_python_oop.rectangle import Rectangle
+from lab_python_oop.circle import Circle
+from lab_python_oop.square import Square
+
+
+def main():
+    r = Rectangle("синего", 17, 17)
+    c = Circle("зеленого", 17)
+    s = Square("красного", 17)
+    print(numpy.e)
+    print(r)
+    print(c)
+    print(s)
+
+if __name__ == "__main__":
+    main()
